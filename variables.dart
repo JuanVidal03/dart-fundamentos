4b1f30f2b611eb8df53f126479cddf49af8d9fb4
Juan Vidal <juanmvg2003@gmail.com>
2024-05-09 12:29:54 -0500

    variables

---
+++ variables.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: unused_local_variable
+
+void main(){
+  // final hace referencia a que ese va a serel ultimo tipo de dato que va a tener la variable, eso quiere decir que despues de ahi no va a acambiar
+  final String pokemon = "Ditto";
+  final int hp = 100;
+  final isALive = true;
+  // las listas se les puede hacer el tipo de dato que van a tener dentro de si
+  final List<String> abilities = ['impostor'];
+  final List<String> sprites = ['ditto/front.png', 'ditto/back.png'];
+
+  // dynamic puede ser cualquier tipo de dato
+  dynamic variableDinamica = 'asdasd';
+
+}
