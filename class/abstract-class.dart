d91277d2b335ac692d08e2ad999a9c65f0716573
Juan Vidal <juanmvg2003@gmail.com>
2024-05-23 08:06:39 -0500

    extends

--- class/abstract-class.dart
+++ class/abstract-class.dart
@@ -1,6 +1,20 @@
+// ignore_for_file: unused_local_variable
+
 void main(){
 
-  // final EnergyPlant windPlant = EnergyPlant();
+  final EnergyPlant windPlant = WindPlant(initialEnergy: 23.2);
+  print(windPlant);
+
+}
+
+
+double chargePhone( EnergyPlant plant ){
+
+  if (plant.energyLeft < 0) {
+    throw Exception('Not enough energy');
+  }
+
+  return plant.energyLeft -10;
 
 }
 
@@ -18,6 +32,26 @@ abstract class EnergyPlant {
   });
 
   // metodo del consumo de energia
-  void consumeEnergy( double amount );
+  void consumeEnergy( double amount ){
+    throw UnimplementedError();
+  }
 
+}
+
+
+// herencia de las clases
+class WindPlant extends EnergyPlant{
+
+
+  // constructor
+  WindPlant({ required double initialEnergy })
+    // llamando al constructor del padre
+    : super(energyLeft: initialEnergy, type: PlantType.wind);
+  
+
+  // accediendo al metodo de la calse padre
+  @override
+  void consumeEnergy(double amount){
+    energyLeft -= amount;
+  }
 }
